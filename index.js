bfed696c8a467c8ec1da78c27579788d57f13851
이성일 <modolee@soblo.io>
2020-02-06 18:51:39 +0900

    babel, docker 설정

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hey you');
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+    Query: {
+        hello: (_, { name }) => `Hello ${name || 'World'}`,
+    },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))

e97e9ffa0c2794493c8cc67cc3b6200825c678a4
이성일 <modolee@soblo.io>
2020-02-06 19:05:35 +0900

    schema, resolvers 추가

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga';
+import resolvers from "./graphql/resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers: resolvers
+});
 
-const resolvers = {
-    Query: {
-        hello: (_, { name }) => `Hello ${name || 'World'}`,
-    },
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))

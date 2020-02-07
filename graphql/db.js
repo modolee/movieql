e3f669bf035478672b80ecbc12cadd3a3bafd2b9
이성일 <modolee@soblo.io>
2020-02-07 12:26:22 +0900

    영화 추가 Mutation 구현

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,51 @@
-export const people = [
+let movies = [
     {
         id: 0,
-        name: "modolee",
-        age: 35,
-        gender: "male"
+        title: "Star Wars - The new one",
+        score: 0.1
     },
     {
         id: 1,
-        name: "Captain America",
-        age: 100,
-        gender: "male"
+        title: "Avengers - The new one",
+        score: 8
     },
     {
         id: 2,
-        name: "Iron Man",
-        age: 40,
-        gender: "male"
+        title: "The Godfather I",
+        score: 99
     },
     {
         id: 3,
-        name: "Black Widow",
-        age: 70,
-        gender: "female"
+        title: "Logan",
+        score: 2
     },
-    {
-        id: 4,
-        name: "Scarlet Witch",
-        age: 23,
-        gender: "female"
-    },
-    {
-        id: 5,
-        name: "Maria Hill",
-        age: 32,
-        gender: "female"
-    }
 ];
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id);
+export const getMovies = () => movies;
 
-    return filteredPeople[0];
-}
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+};
+
+export const addMovie = (title, score) => {
+  const newMovie = {
+      id: movies.length,
+      title,
+      score
+  };
+
+  movies.push(newMovie);
+
+  return newMovie;
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,12 @@
-import { people, getById } from './db';
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 
 const resolvers = {
   Query: {
-      people: () => people,
-      person: (_, { id }) => getById(id)
+      movies: () => getMovies(),
+      movie: (_, { id }) => getById(id)
+  },
+  Mutation: {
+      addMovie: (_, { title, score }) => addMovie(title, score)
   }
 };
 
